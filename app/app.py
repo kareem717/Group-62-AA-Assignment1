6cfda4909ceafb580c3d5ffac0a68cde2526ca73
devshafi <frshafi49@gmail.com>
2024-09-11 20:25:39 -0400

    feat: added sample input example

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        return render_template('index.html', user_input=user_input)
+    return render_template('index.html', user_input=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)

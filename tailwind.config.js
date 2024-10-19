a7088e4d5cf255a4c5b6f8ca7f5b42c0feecd881
chsky1600 <ethanchankowsky@gmail.com>
2024-10-19 00:32:42 -0400

    setup tailwind, add updated index page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/templates/**/*.html',
+    './app/**/*.py',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

d80249bbae41d3a195750ea20f0136a9ad706175
MrNuts62 <datbaconthow@gmail.com>
2024-12-04 21:05:08 -0500

    updated selenium script

--- Assignment-6/e2e_test.py
+++ Assignment-6/e2e_test.py
@@ -13,7 +13,8 @@ driver.find_element(By.XPATH, "//input[@type='checkbox']").click()
 driver.find_element(By.XPATH, "//button[contains(text(), 'Sign Up')]").click()
 time.sleep(2)
 
-# assert "Sign Up Successful" in driver.page_source
+assert "Sign In" in driver.page_source
+print("Sign Up Successful")
 
 driver.get("http://localhost:5007/login")
 driver.find_element(By.NAME, "email").send_keys("testuser@example.com")
@@ -21,11 +22,13 @@ driver.find_element(By.NAME, "password").send_keys("testpassword")
 driver.find_element(By.XPATH, "//button[contains(text(), 'Sign In')]").click()
 time.sleep(2)
 
-# assert "Welcome" in driver.page_source
+assert "Logout" in driver.page_source
+print("Login Successful")
 
 driver.find_element(By.XPATH, "//a[contains(@class, 'bg-gray-200')]").click()
 time.sleep(2)
 
-# assert "Purchase" in driver.page_source
+assert "Purchase" in driver.page_source
+print("Flights Page Loaded")
 
 driver.quit()
